845265df83a6bf5244e706df83301689ff32075d
Jason <jason_yu25@milton.edu>
2023-06-05 14:17:14 -0400

    finished blackjack!

--- src/Card.java
+++ src/Card.java
@@ -4,9 +4,62 @@ public class Card {
     public String name;
     public boolean isDealt;
 
-    public Card(int pSuit,int pName){
+    public Card(int pSuit,int pName){ //constructor has no void and same name as class
+
         if(pSuit == 0){
             suit = "Diamond";
         }
+        if(pSuit == 1){
+            suit = "Club";
+        }
+        if(pSuit == 2){
+            suit = "Spades";
+        }
+        if(pSuit == 3){
+            suit = "Hearts";
+        }
+        if(pName ==1){
+            name = "ACE";
+        }
+        if(pName ==2){
+            name = "2";
+        }
+        if(pName ==3){
+            name = "3";
+        }
+        if(pName ==4){
+            name = "4";
+        }
+        if(pName ==5){
+            name = "5";
+        }
+        if(pName ==6){
+            name = "6";
+        }
+        if(pName ==7){
+            name = "7";
+        }
+        if(pName ==8){
+            name = "8";
+        }
+        if(pName ==9){
+            name = "9";
+        }
+        if(pName ==10){
+            name = "10";
+        }
+        if(pName ==11){
+            name = "JACK";
+        }
+        if(pName ==12){
+            name = "QUEEN";
+        }
+        if(pName ==13){
+            name = "KING";
+        }
+        System.out.println("You made the " + name + " of " + suit);
+    }
+    public void printCard(){
+        System.out.println("This is the " + name + " of " + suit + " it is dealt " + isDealt);
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,45 @@
 public class Game {
+    public int a;
+    public int b;
+    public Card card1;
+    public Card[] deck;
+    public int count=0;
+
+    public static void main(String[] args){
+        Game myGame;
+        myGame = new Game();
+    }
+    public Game(){
+        System.out.println("Game constructor");
+        deck = new Card[52];
+        for(int w=0;w<4;w++) {
+            for (int x = 1; x < 14; x++) {
+                deck[count] = new Card(w, x);
+                count++;
+            }
+
+        }
+//        deck[0] = new Card(1,1);
+//        deck[0].print();
+        card1=new Card(1,1);
+       // card1.suit = "spades";
+        card1.printCard();
+        shuffle();
+    }
+    public void shuffle() {
+        Card[] shuffledDeck = new Card[52];
+        for(int f = 0; f<shuffledDeck.length;f++){
+            int randomCard = (int)(Math.random()*52);
+            Card shufflecard = deck[randomCard];
+            while(deck[randomCard].isDealt==true){
+                shufflecard.printCard();
+                randomCard = (int)(Math.random()*52);
+                shufflecard = deck[randomCard];
+            }
+//            deck[randomCard].isDealt==true;
+            shufflecard.printCard();
+            shuffledDeck[f] = shufflecard;
+        }
+        deck = shuffledDeck;
+    }
 }
